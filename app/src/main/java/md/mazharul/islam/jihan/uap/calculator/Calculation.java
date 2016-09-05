584b73052cd2dc679f0711b70a84e1b7a8e430a6
jihanislam007 <jihanislam007@gmail.com>
2016-09-06 01:20:34 +0600

    Initial commit

---
+++ app/src/main/java/md/mazharul/islam/jihan/uap/calculator/Calculation.java
@@ -0,0 +1,34 @@
+package md.mazharul.islam.jihan.uap.calculator;
+
+/**
+ * Created by Jihan on 6/21/2016.
+ */
+public class Calculation {
+
+
+    double numberOne;
+    double numberTwo;
+    double result;
+
+    public double getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(double numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public double getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(double numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public  double add(){
+        result = numberOne+numberTwo;
+        return add();
+    }
+
+}
